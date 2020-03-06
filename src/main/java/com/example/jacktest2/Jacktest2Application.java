6b89d84e1ea262a3a3dca729b2c71e692c404361
Jack <gudtjrwns@gmail.com>
2020-03-06 17:26:47 +0900

    1 commit [06 March 20]
    프로젝트 생성

---
+++ src/main/java/com/example/jacktest2/Jacktest2Application.java
@@ -0,0 +1,13 @@
+package com.example.jacktest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jacktest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jacktest2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jacktest2/Jacktest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jacktest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jacktest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
