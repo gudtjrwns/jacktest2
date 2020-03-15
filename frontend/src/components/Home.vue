23589b33024fb6004aeb0c16c6070529f6cd76cb
Jack <gudtjrwns@gmail.com>
2020-03-15 09:43:08 +0900

    1 commit [15 March 20]
    Home.vue
    - ui 수정
    - 버튼 경로 수정

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -4,9 +4,10 @@
       <div class="pull-left">
         <div class="form-inline">
           <div class="form-group">
-            <button type="button" class="btn btn-sm btn-info">
-              <i class="far fa-edit"></i> 신규등록
-            </button>
+            <button @click="$router.push('add')" class="btn btn-sm btn-info">신규 등록</button>
+<!--            <button type="button" class="btn btn-sm btn-info">-->
+<!--              <i class="far fa-edit"></i> 신규등록-->
+<!--            </button>-->
           </div>
           <div class="form-group">
             <button type="button" class="btn btn-sm btn-danger">
@@ -31,6 +32,7 @@
       <table class="table table-striped table-bordered table-hover">
         <colgroup>
           <col width="8%">
+          <col width="8%">
           <col width="*%">
           <col width="15%">
           <col width="15%">
@@ -39,6 +41,7 @@
         </colgroup>
         <thead class="thead-dark text-center">
         <tr>
+          <td><input type="checkbox" /></td>
           <td>No</td>
           <td>제목</td>
           <td>작성자</td>
@@ -49,12 +52,13 @@
         </thead>
         <tbody>
         <tr v-for="notice in notices" :key="notice.id">
+          <td><input type="checkbox" /></td>
           <td>{{notice.id}}</td>
           <td>{{notice.title}}</td>
           <td>{{notice.writer}}</td>
           <td>{{notice.credate}}</td>
-          <td>수정</td>
-          <td>삭제</td>
+          <td><button @click="$router.push('edit')" class="btn btn-sm btn-info">수정</button></td>
+          <td><button @click="deleteNotice(notice.id)" class="btn btn-sm btn-danger">삭제</button></td>
         </tr>
         </tbody>
       </table>
@@ -81,6 +85,11 @@
         .catch(e => {
           console.log('error : ', e)
         });
+    },
+    methods: {
+      deleteNotice(index) {
+        console.log(index);
+      }
     }
 
   }
